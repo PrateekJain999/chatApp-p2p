7cff98291fc5a12082a391f1b3659565b9d9b84b
Prateek Jain <jain22719@gmail.com>
2021-05-13 13:56:19 +0530

    working on user location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,23 @@
 const socket = io()
 
-socket.on('welcome', (data) =>{
+socket.on('message', (data) =>{
     console.log(data)
 })
 
 document.querySelector('#message-form').addEventListener('submit', (e)=>{
     e.preventDefault()
-    const message = document.querySelector('input').value
+    const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message);
+})
+
+document.querySelector('#send-location').addEventListener('click', ()=>{
+    if(!navigator.geolocation)
+    {
+        return alert('Geolocation is not supported')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {latitude : position.coords.latitude, longitude : position.coords.longitude})
+    })
 })
--- src/index.js
+++ src/index.js
@@ -17,9 +17,19 @@ io.on('connection', (socket)=>{
     console.log('User Connected');
 
     socket.emit('message', 'welcome')
+    socket.broadcast.emit('message', 'A new user has joined')
 
     socket.on('sendMessage', (message) =>{
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) =>{
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('message', coords)
+    })
+
+    socket.on('disconnect', () =>{
+        io.emit('message', 'user has left')
     })
 })
 
